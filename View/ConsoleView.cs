7dc15b8b7263dbcaacacf340785b2fdc9b4f164e
Ryan-Vreeke <rvreeke1234@gmail.com>
2021-12-26 23:24:33 -0700

    added console view

---
+++ View/ConsoleView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Connection;
+using System.Threading.Tasks;
+using Model;
+
+namespace View
+{
+    
+    class ConsoleView
+    {
+        static async Task Main(string[] args)
+        {
+            ApiConnection conn = new ApiConnection();
+            string command;
+            bool quit = false;
+            while (!quit)
+            {
+                System.Console.Write("Enter Command:");
+                command = Console.ReadLine();
+                switch (command)
+                {
+                    case "/manga":
+                        await conn.GetComics();
+                        conn.Process();
+                        break;
+                    case "/quit":
+                        quit = true;
+                        break;
+                }
+            }
+
+        }
+    }
+}
